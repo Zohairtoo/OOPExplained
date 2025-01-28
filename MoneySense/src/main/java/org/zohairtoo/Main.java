88d5300121af5e0672674b7e1e3def82bda9297e
zohair ahmed <zohair.ahmed@onespan.com>
2025-01-27 21:28:23 -0500

    initial commit

---
+++ MoneySense/src/main/java/org/zohairtoo/Main.java
@@ -0,0 +1,20 @@
+package org.zohairtoo;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, from Zohair!");
+        System.out.println("This app is basically for personal use to track personal state and stages of financial aspects throughout a life span");
+        System.out.println("It is not just simple financial tracker, but it provide a unique connectivity from personal financial state and project what needs to be done with respect to it for future.");
+        System.out.println("It will gather all sort of Information from the user, but then");
+        System.out.println("It will leverage AI to evaluate and suggest on the basis of user's age, that what needs to be done");
+        System.out.println("It will have Microservice endpoints with Docker and Kubernetes as a backend");
+        System.out.println("Some of the prominent parts");
+        System.out.println("1. Banking");
+        System.out.println("2. Investing");
+        System.out.println("3. Insurance");
+        System.out.println("4. Mortgage/Loans");
+        System.out.println("5. Credit Cards");
+
+
+    }
+}
