98a0470d3cfe1a025dd5d57d67e6704d0436108c
Zohairtoo <zohairtoo@gmail.com>
2025-01-27 22:24:44 -0500

    sentence completion

--- MoneySense/src/main/java/org/zohairtoo/Main.java
+++ MoneySense/src/main/java/org/zohairtoo/Main.java
@@ -5,7 +5,7 @@ public class Main {
         System.out.println("Hello, from Zohair!");
         System.out.println("This app is basically for personal use to track personal state and stages of financial aspects throughout a life span");
         System.out.println("It is not just simple financial tracker, but it provide a unique connectivity from personal financial state and project what needs to be done with respect to it for future.");
-        System.out.println("It will gather all sort of Information from the user, but then");
+        System.out.println("It will gather all sort of Information from the user, but then wit hthe power of AI it will evaluate");
         System.out.println("It will leverage AI to evaluate and suggest on the basis of user's age, that what needs to be done");
         System.out.println("It will have Microservice endpoints with Docker and Kubernetes as a backend");
         System.out.println("Some of the prominent parts");
